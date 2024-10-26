d43c4a6b96a092bbfe269266ec484edbace98bdf
edyta godniak <edyta.godniak@protonmail.ch>
2024-10-26 23:31:33 +0200

    test for starting the game that is already started

--- src/test/java/org/godniakedyta/GameServiceImplTest.java
+++ src/test/java/org/godniakedyta/GameServiceImplTest.java
@@ -3,6 +3,7 @@ package org.godniakedyta;
 import org.godniakedyta.game.Game;
 import org.godniakedyta.game.GameServiceImpl;
 import org.godniakedyta.game.GameStorage;
+import org.godniakedyta.game.Rivals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -11,8 +12,10 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import java.time.ZonedDateTime;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -41,17 +44,17 @@ class GameServiceImplTest {
     @Test
     void GIVEN_allParamsProvided_WHEN_start_THEN_newGameAddedToStorage() {
         //given
-        var teamHome = "Austria";
-        var teamAway = "Belgium";
+        var homeTeam = "Austria";
+        var awayTeam = "Belgium";
         //when
         ZonedDateTime timeBeforeExecution = ZonedDateTime.now();
-        gameServiceImpl.start(teamHome, teamAway);
+        gameServiceImpl.start(homeTeam, awayTeam);
         ZonedDateTime timeAfterExecution = ZonedDateTime.now();
         //then
         verify(gameStorage, times(1)).add(gameCaptor.capture());
         var gamePassed = gameCaptor.getValue();
-        assertEquals(teamHome, gamePassed.getRivals().homeTeam());
-        assertEquals(teamAway, gamePassed.getRivals().awayTeam());
+        assertEquals(homeTeam, gamePassed.getRivals().homeTeam());
+        assertEquals(awayTeam, gamePassed.getRivals().awayTeam());
         assertEquals(0, gamePassed.getHomeTeamScore());
         assertEquals(0, gamePassed.getAwayTeamScore());
         assertEquals(0, gamePassed.getTotalScore());
@@ -72,6 +75,16 @@ class GameServiceImplTest {
     void GIVEN_wrongParams_WHEN_start_THEN_IllegalArgumentExpectionIsThrown(String homeTeam, String awayTeam) {
         //then
         assertThrows(IllegalArgumentException.class, () -> gameServiceImpl.start(homeTeam, awayTeam));
+    }
+
+    @ParameterizedTest(name = "{0} as homeTeam and {1} as awayTeam")
+    @CsvSource({"Austria,Belgium",
+            "Belgium,Austria"})
+    void GIVEN_allParamsProvided_WHEN_startAndGameAlreadyExists_THEN_UnsupportedOperationExceptionIsThrown(String homeTeam, String awayTeam) {
+        //when
+        Mockito.when(gameStorage.findGameByRivals(any())).thenReturn(Optional.of(Game.builder().build()));
+        //then
+        assertThrows(UnsupportedOperationException.class, () -> gameServiceImpl.start(homeTeam, awayTeam));
     }
 
 }
