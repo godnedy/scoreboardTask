f3ba95a54b4d1673b9d38d3d5ab7bd5f30927428
edyta godniak <edyta.godniak@protonmail.ch>
2024-10-26 22:32:19 +0200

    edge cases tests for start game added

--- src/test/java/org/godniakedyta/GameServiceImplTest.java
+++ src/test/java/org/godniakedyta/GameServiceImplTest.java
@@ -5,13 +5,14 @@ import org.godniakedyta.game.GameServiceImpl;
 import org.godniakedyta.game.GameStorage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 
 import java.time.ZonedDateTime;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
@@ -55,6 +56,21 @@ class GameServiceImplTest {
         assertEquals(0, gamePassed.getAwayTeamScore());
         assertEquals(0, gamePassed.getTotalScore());
         assertTrue(timeBeforeExecution.isBefore(gamePassed.getStartTime()) && timeAfterExecution.isAfter(gamePassed.getStartTime()));
+    }
+
+    @ParameterizedTest(name = "{0} as homeTeam and {1} as awayTeam")
+    //etc. this is not a final list of all edge cases
+    @CsvSource({"Austria,Austria",
+            "Austria,''",
+            "'',''",
+            "'',Austria",
+            ",",
+            "Austria,",
+            ",Austria",
+            "'',"})
+    void GIVEN_wrongParams_WHEN_start_THEN_IllegalArgumentExpectionIsThrown(String homeTeam, String awayTeam) {
+        //then
+        assertThrows(IllegalArgumentException.class, () -> gameServiceImpl.start(homeTeam, awayTeam));
     }
 
 }
